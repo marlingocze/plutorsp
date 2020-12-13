bd32bd70acb256382d9cf2a18999b69e82984a21
Martin Linka <mli@revolgy.com>
2020-12-13 12:31:33 +0100

    Add: install wordpress for azure

--- app/base_nextcloud/apps/photos/l10n/mn.js
+++
@@ -1,9 +0,0 @@
-OC.L10N.register(
-    "photos",
-    {
-    "Photos" : "Зурагнууд",
-    "Favorites" : "Онцолсон",
-    "Shared with you" : "тантай хуваалцсан",
-    "Settings" : "Тохиргоо"
-},
-"nplurals=2; plural=(n != 1);");
