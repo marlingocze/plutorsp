6da077a9138011f0c7eafd6ff28de7f2ca2c02c6
Martin Linka <mli@revolgy.com>
2020-12-21 00:56:54 +0100

    rm unused files

--- from_azure/provision/install_base.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-RUN_AS_USER="www-data"
-NEXTCLOUD_RELEASE="https://github.com/nextcloud/server/archive/v20.0.1RC1.zip"
-
-sudo -u www-data php occ  maintenance:install --database \
- "mysql" --database-name "nextcloud"  --database-user "root" --database-pass \
- "password" --admin-user "admin" --admin-pass "password"
